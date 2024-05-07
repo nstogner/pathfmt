ac5d0f326297eaf1dd336e25cc2559d6ecbaf680
Nick Stogner <nicholas.stogner@gmail.com>
2024-05-07 15:38:09 -0400

    Rename files

--- parse_test.go
+++ parse_test.go
@@ -1,152 +1,34 @@
-package pathfmt
+package pathfmt_test
 
-import "testing"
+import (
+	"fmt"
+	"log"
 
-func TestNew(t *testing.T) {
-	cases := []struct {
-		input    string
-		expected []namedPart
-	}{
-		{
-			input: "/items/{id}/subitems/{subid}",
-			expected: []namedPart{
-				{
-					name:  "id",
-					index: 1,
-				},
-				{
-					name:  "subid",
-					index: 3,
-				},
-			},
-		},
-	}
+	"github.com/nstogner/pathfmt"
+)
 
-	for _, c := range cases {
-		t.Run(c.input, func(t *testing.T) {
-			tmpl := New(c.input)
+func ExampleToMap() {
+	f := pathfmt.New("/api/v1/users/{id}")
 
-			if len(tmpl.namedParts) != len(c.expected) {
-				t.Fatalf("expected %d named parts, got %d", len(c.expected), len(tmpl.namedParts))
-			}
+	m := f.ToMap("/api/v1/users/123")
 
-			for i, np := range tmpl.namedParts {
-				if np != c.expected[i] {
-					t.Fatalf("expected named part %v, got %v", c.expected[i], np)
-				}
-			}
-		})
-	}
-}
-func TestToMap(t *testing.T) {
-	cases := []struct {
-		name     string
-		template string
-		input    string
-		expected map[string]string
-	}{
-		{
-			name:     "exact-match",
-			template: "/items/{id}/subitems/{subid}",
-			input:    "/items/123/subitems/456",
-			expected: map[string]string{
-				"id":    "123",
-				"subid": "456",
-			},
-		},
-		{
-			name:     "shorter-input",
-			template: "/items/{id}/subitems/{subid}",
-			input:    "/items/123/subitems",
-			expected: map[string]string{
-				"id": "123",
-			},
-		},
-		{
-			name:     "longer-input",
-			template: "/items/{id}/subitems/{subid}",
-			input:    "/items/123/subitems/456/extra",
-			expected: map[string]string{
-				"id":    "123",
-				"subid": "456",
-			},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			template := New(c.template)
-			m := template.ToMap(c.input)
-
-			if len(m) != len(c.expected) {
-				t.Fatalf("expected %d values, got %d", len(c.expected), len(m))
-			}
-
-			for k, v := range c.expected {
-				if m[k] != v {
-					t.Fatalf("expected %s=%s, got %s=%s", k, v, k, m[k])
-				}
-			}
-		})
-	}
+	fmt.Println(m)
+	// Output: map[id:123]
 }
 
-func TestToStruct(t *testing.T) {
-	const pathTemplate = "/a/{a}/b/{b}/c/{c}/d/{d}"
-	type MyPath struct {
-		A string  `path:"a"`
-		B int     `path:"b"`
-		C float64 `path:"c"`
-		D bool    `path:"d"`
+func ExampleToStruct() {
+	type UserIdentifier struct {
+		OrgNum int    `path:"org_num"`
+		ID     string `path:"id"`
 	}
 
-	cases := []struct {
-		name     string
-		input    string
-		expected MyPath
-	}{
-		{
-			name:  "all-values",
-			input: "/a/abc/b/123/c/3.14/d/true",
-			expected: MyPath{
-				A: "abc",
-				B: 123,
-				C: 3.14,
-				D: true,
-			},
-		},
-		{
-			name:  "missing-values",
-			input: "/a/abc/b/123",
-			expected: MyPath{
-				A: "abc",
-				B: 123,
-			},
-		},
-		{
-			name:  "extra-values",
-			input: "/a/abc/b/123/c/3.14/d/true/e/extra",
-			expected: MyPath{
-				A: "abc",
-				B: 123,
-				C: 3.14,
-				D: true,
-			},
-		},
+	f := pathfmt.New("/organizations/{org_num}/users/{id}")
+
+	var u UserIdentifier
+	if err := f.ToStruct("/organizations/123/users/nick", &u); err != nil {
+		log.Fatal(err)
 	}
 
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			template := New(pathTemplate)
-			var path MyPath
-			err := template.ToStruct(c.input, &path)
-			if err != nil {
-				t.Fatalf("unexpected error: %v", err)
-			}
-
-			if path != c.expected {
-				t.Fatalf("expected %v, got %v", c.expected, path)
-			}
-		})
-	}
+	fmt.Println(u.OrgNum, u.ID)
+	// Output: 123 nick
 }
